da6122da923bf152523b57ac3f52b537e1ec8861
Mohamed Mosatafa Motawie <81030289+motawie0@users.noreply.github.com>
2024-07-23 19:42:20 +0300

    Update decoder_timestamps_utils.py
    Signed-off-by: Mohamed Mosatafa Motawie <81030289+motawie0@users.noreply.github.com>

--- nemo/collections/asr/parts/utils/decoder_timestamps_utils.py
+++ nemo/collections/asr/parts/utils/decoder_timestamps_utils.py
@@ -756,6 +756,8 @@ class ASRDecoderTimeStamps:
         """
         beams = self.beam_search_decoder.decode_beams(logprob, beam_width=self.ctc_decoder_params['beam_width'])
         print("motawieeee: ", beams)
+        print("motawieeee: ", beams[0])
+        print("motawieeee: ", beams[0][2])
         word_ts_beam, words_beam = [], []
         for idx, (word, _) in enumerate(beams[0][2]):
             ts = self.get_word_ts_from_wordframes(idx, beams[0][2], self.model_stride_in_secs, onset_delay_in_sec)
